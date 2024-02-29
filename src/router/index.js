a0ab09fa587f0ad573a94eaf6b7720b0e4fdefae
model <2804485264@qq.com>
2024-02-29 17:23:51 +0800

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Compare from '../views/Compare.vue'
 import Vin from '../views/Vin.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
